255e39bc893ed968e4272424f6204f9eb17bcb06
SilverSynch <isaacgwitherspoon@gmail.com>
2023-06-20 10:48:46 -0700

    Add tests for new features.

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,9 +1,22 @@
-import org.junit.jupiter.api.Assertions;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
-    
+
+    // Test that negative numbers throw an IllegalArgumentException.
+    @Test
+    public void testNegativeNumbers1() {
+        String testData = "2, -1";
+
+        assertThrows(IllegalArgumentException.class, () -> {StringCalculator.add(testData);});
+    }
+
+    // Test that numbers greater than 1000 are ignored.
+    @Test
+    public void testNumbersOver1000a() {
+        String testData = "2000, 10";
+
+        assertEquals(10, StringCalculator.add(testData));
+    }
 }
